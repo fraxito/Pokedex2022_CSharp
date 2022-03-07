bfbeda22f38911ac3cd9ffd4dbf6d299a4168ce2
CETYSDAM <CETYSDAM@DESKTOP-HMUGL0C>
2022-03-07 19:56:39 +0100

    Agregar archivos de proyecto.

---
+++ Pokedex2022_CSharp/VentanaPrincipal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokedex2022_CSharp
+{
+    public partial class VentanaPrincipal : Form
+    {
+        Conexion miConexion = new Conexion();
+        DataTable misPokemons = new DataTable();
+        int idActual = 1; //guarda el id del pokemon que se está viendo
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void izquierda_Click(object sender, EventArgs e)
+        {
+            idActual--;
+            misPokemons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
+        }
+
+        private Image convierteBlobAImagen(byte[] img) {
+            MemoryStream ms = new System.IO.MemoryStream(img);
+            return (Image.FromStream(ms));
+        }
+
+        private void derecha_Click(object sender, EventArgs e)
+        {
+            idActual++;
+            misPokemons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
+            pictureBox1.Image = convierteBlobAImagen((byte[])misPokemons.Rows[0]["imagen"]);
+        }
+
+
+    }
+}
